344eba865488ba9564428f852a637fc9915cc401
Stefan <steve260897@gmail.com>
2020-04-15 12:08:09 +0200

    Linted the shit out of it

--- src/EX1/RuleLearner.java
+++ src/EX1/RuleLearner.java
@@ -1,12 +1,11 @@
 package EX1;
 
-import weka.classifiers.AbstractClassifier;
+import weka.classifiers.evaluation.Evaluation;
 import weka.classifiers.rules.ConjunctiveRule;
 import weka.classifiers.rules.JRip;
 import weka.core.Attribute;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
-import weka.classifiers.evaluation.Evaluation;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -18,8 +17,8 @@ public class RuleLearner {
 
 	public static void main(final String[] args) throws Exception {
 		final Set<String> files = getFileNames();
-		String result = String.format("%-15s\t %-11s\t %-10s\t %-10s\n", "Dataset", "JRip", "JRip noPruning", "ConjunctiveRule");
-		Integer[] totalRanks = {0,0,0};
+		final StringBuilder result = new StringBuilder(String.format("%-15s\t %-11s\t %-10s\t %-10s\n", "Dataset", "JRip", "JRip noPruning", "ConjunctiveRule"));
+		final int[] totalRanks = {0, 0, 0};
 
 		for (final String file : files) {
 
@@ -29,52 +28,52 @@ public class RuleLearner {
 			final Instances data = loadDataset(file);
 
 			final JRip pruning = getRipper(data, true);
-			Evaluation evalPruning = new Evaluation(data);
+			final Evaluation evalPruning = new Evaluation(data);
 			evalPruning.crossValidateModel(pruning, data, 10, new Random(0));
 			System.out.println(pruning);
 			System.out.println(evalPruning.pctCorrect());
 			System.out.println("###########################################################");
 
 			final JRip noPruning = getRipper(data, false);
-			Evaluation evalNoPruning = new Evaluation(data);
+			final Evaluation evalNoPruning = new Evaluation(data);
 			evalNoPruning.crossValidateModel(noPruning, data, 10, new Random(0));
 			System.out.println(noPruning);
 			System.out.println(evalNoPruning.pctCorrect());
 			System.out.println("###########################################################");
 
 			final ConjunctiveRule rule = getConjunctiveRule(data);
-			Evaluation evalRule = new Evaluation(data);
+			final Evaluation evalRule = new Evaluation(data);
 			evalRule.crossValidateModel(rule, data, 10, new Random(0));
 			System.out.println(rule);
 			System.out.println(evalRule.pctCorrect());
 			System.out.println("###########################################################");
 
-			List<Double> accs = List.of(evalPruning.pctCorrect(), evalNoPruning.pctCorrect(), evalRule.pctCorrect());
-			List<Double> sorted = accs.stream()
-					.sorted(Comparator.reverseOrder())
-					.collect(Collectors.toList());
-			List<Integer> ranks = accs.stream()
-					.map(e -> sorted.indexOf(e)+1)
-					.collect(Collectors.toList());
-			result += String.format("%-15s\t %f (%d)\t %f (%d)\t %f (%d)\n", data.relationName(),
-					accs.get(0), ranks.get(0), accs.get(1), ranks.get(1), accs.get(2), ranks.get(2));
+			final List<Double> accs = List.of(evalPruning.pctCorrect(), evalNoPruning.pctCorrect(), evalRule.pctCorrect());
+			final List<Double> sorted = accs.stream()
+					                            .sorted(Comparator.reverseOrder())
+					                            .collect(Collectors.toList());
+			final List<Integer> ranks = accs.stream()
+					                            .map(e -> sorted.indexOf(e) + 1)
+					                            .collect(Collectors.toList());
+			result.append(String.format("%-15s\t %f (%d)\t %f (%d)\t %f (%d)\n", data.relationName(),
+					accs.get(0), ranks.get(0), accs.get(1), ranks.get(1), accs.get(2), ranks.get(2)));
 			totalRanks[0] += ranks.get(0);
 			totalRanks[1] += ranks.get(1);
 			totalRanks[2] += ranks.get(2);
 		}
 
 		System.out.println();
-		double n = files.size();
-		double[] avgRanks = {totalRanks[0]/n, totalRanks[1]/n, totalRanks[2]/n};
-		result += "-------------------------------------------------------------------\n";
-		result += String.format("%-15s\t %f \t\t %f \t\t %f\n", "AVG RANK",
-				avgRanks[0], avgRanks[1], avgRanks[2]);
+		final double n = files.size();
+		final double[] avgRanks = {totalRanks[0] / n, totalRanks[1] / n, totalRanks[2] / n};
+		result.append("-------------------------------------------------------------------\n");
+		result.append(String.format("%-15s\t %f \t\t %f \t\t %f\n", "AVG RANK",
+				avgRanks[0], avgRanks[1], avgRanks[2]));
 		System.out.println(result);
 
 		performFriedmanNemenyiTests(avgRanks, n);
 	}
 
-	private static void performFriedmanNemenyiTests(double[] avgRanks, double n) {
+	private static void performFriedmanNemenyiTests(final double[] avgRanks, final double n) {
 		System.out.println("Perform the Friedman statistics test:");
 		// Perform the Friedman statistics test
 		// E.g. k = 3, N = 10
@@ -82,10 +81,10 @@ public class RuleLearner {
 		// Chi²F = 12*10 / 3*4  *  ( SUM(avgRanks²) - 3*4 / 4 )
 		// Chi²(0.95,2) = 5.991   -> see https://people.richland.edu/james/lecture/m170/tbl-chi.html
 
-		double k = avgRanks.length;
-		double sumAvgRanks2 = DoubleStream.of(avgRanks).map(a -> a*a).sum();
-		double chi2F = 12*n / k*(k+1) * (sumAvgRanks2 - k*(k+1)*(k+1) / 4);
-		double chi2 = 5.991; // Todo: create lookup Chi²(0.95,k-1)
+		final double k = avgRanks.length;
+		final double sumAvgRanks2 = DoubleStream.of(avgRanks).map(a -> a * a).sum();
+		final double chi2F = 12 * n / k * (k + 1) * (sumAvgRanks2 - k * (k + 1) * (k + 1) / 4);
+		final double chi2 = 5.991; // Todo: create lookup Chi²(0.95,k-1)
 		if (chi2 < chi2F) {
 			System.out.println(String.format("χ²(0.95;2) = %f < %f = χ²F", chi2, chi2F));
 			System.out.println("Null hypotheses successfully rejected with p = 0.95!");
@@ -93,11 +92,10 @@ public class RuleLearner {
 			System.out.println("Perform the Nemenyi post-hoc test:");
 			// Perform the Nemenyi post-hoc test (which can be performed if the null hypothesis of the Friedman is rejected)
 			// q_alpha_0.05_#c3 = 2.343
-			double q_alpha = 2.343; // Todo: lookup q_alpha for p and k
-			double CD = q_alpha * Math.sqrt(k*(k+1)/(6*n)); // Critical Distance between pairs of avgRanks
+			final double q_alpha = 2.343; // Todo: lookup q_alpha for p and k
+			final double CD = q_alpha * Math.sqrt(k * (k + 1) / (6 * n)); // Critical Distance between pairs of avgRanks
 			System.out.println(String.format("CD = %f", CD));
 			// Todo: compare avgRanks all pairs of classifiers and check significant difference by CriticalDistance CD
-
 		} else {
 			System.out.println(String.format("χ²(0.95;2) = %f >= %f = χ²F", chi2, chi2F));
 			System.out.println("Null hypotheses could NOT be rejected with p = 0.95!");
@@ -114,12 +112,12 @@ public class RuleLearner {
 		final Instances data = DataSource.read(path);
 
 		// For some Datasets the class attribute is not the last one, and for some it's not called 'class'
-		if (data.classIndex() == -1)  {
+		if (data.classIndex() == -1) {
 			Attribute attClass = data.attribute("class");
 			if (attClass == null) {
 				attClass = data.attribute("Class");
 			}
-			if (attClass != null){
+			if (attClass != null) {
 				data.setClassIndex(attClass.index());
 			} else {
 				data.setClassIndex(data.numAttributes() - 1);
@@ -133,10 +131,6 @@ public class RuleLearner {
 		ripper.setUsePruning(usePruning);
 		ripper.buildClassifier(data);
 		return ripper;
-	}
-
-	private static int numberOfRules(final JRip ripper) {
-		return ripper.getRuleset().size();
 	}
 
 	private static Set<String> getFileNames() {
