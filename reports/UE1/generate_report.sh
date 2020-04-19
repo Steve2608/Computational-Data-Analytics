bf32ea47141bbacbc437d88b2018d21609fdc6fb
Stefan <steve260897@gmail.com>
2020-04-19 21:41:50 +0200

    adding option to create html output

--- reports/UE1/generate_report.sh
+++ reports/UE1/generate_report.sh
@@ -1,1 +1,7 @@
-pandoc report1.md -o report1.pdf --from markdown --template eisvogel --pdf-engine=xelatex -V fontsize=11pt -V geometry:landscape
+if [[ "$1" == "pdf" ]]; then
+  pandoc report1.md -o report1.html --pdf-engine=xelatex -V fontsize=11pt -V geometry:landscape
+elif [[ "$1" == "html" ]]; then
+  pandoc report1.md -o report1.html --template GitHub.html5 --metadata title="Computational Data Analytics, UE1" --self-contained
+else
+  echo "Unknown parameter: $1"
+fi
