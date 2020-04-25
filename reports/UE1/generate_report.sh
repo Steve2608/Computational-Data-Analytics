9158f64ad0780b8c27602baa5a9c070963ef998c
Stefan B <steve260897@gmail.com>
2020-04-25 10:38:23 +0200

    Fixed pdf output command

--- reports/UE1/generate_report.sh
+++ reports/UE1/generate_report.sh
@@ -1,5 +1,5 @@
 if [[ "$1" == "pdf" ]]; then
-  pandoc report1.md -o report1.html --pdf-engine=xelatex -V fontsize=11pt -V geometry:landscape
+  pandoc report1.md -o report1.pdf --template eisvogel --pdf-engine=xelatex -V fontsize=11pt -V geometry:landscape
 elif [[ "$1" == "html" ]]; then
   pandoc report1.md -o report1.html --template GitHub.html5 --metadata title="Computational Data Analytics, UE1" --self-contained
 else
