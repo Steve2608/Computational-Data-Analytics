db744692e56e3123cb3e110286e17302ced26ccf
Stefan Brandl <steve260897@gmail.com>
2020-05-01 17:46:14 +0200

    updated pdf-compilation arguments

--- reports/UE1/generate_report.sh
+++ reports/UE1/generate_report.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ "$1" == "pdf" ]]; then
-  pandoc report1.md -o report1.pdf --template eisvogel --pdf-engine=xelatex -V fontsize=11pt -V geometry:landscape
+  pandoc report1.md -o report1.pdf --template eisvogel --pdf-engine=xelatex -V fontsize=10pt -V geometry:landscape
 elif [[ "$1" == "html" ]]; then
   pandoc report1.md -o report1.html --template GitHub.html5 --metadata title="Computational Data Analytics, UE1" --self-contained
 else
