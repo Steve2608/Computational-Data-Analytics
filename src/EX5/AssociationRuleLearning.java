dc454e3531ca65bf4e30f158266d18c36663d601
Stefan Brandl <steve260897@gmail.com>
2020-05-23 21:59:00 +0200

    Implemented EX5

--- src/EX5/AssociationRuleLearning.java
+++ src/EX5/AssociationRuleLearning.java
@@ -7,31 +7,65 @@ import static util.Util.loadDataset;
 
 public class AssociationRuleLearning {
 
-	private static final String WEATHER_NOMINAL = "data/weather.nominal.arff";
+	private static final String WEATHER_PATH = "data/weather.nominal.arff";
+	private static final String SUPERMARKET_PATH = "data/supermarket.arff";
+	private static Instances WEATHER;
+	private static Instances SUPERMARKET;
 
 	private static final int NUM_RULES = (int) 1e6;
 
-	private AssociationRuleLearning() {
+	static {
+		try {
+			WEATHER = loadDataset(WEATHER_PATH);
+			SUPERMARKET = loadDataset(SUPERMARKET_PATH);
+		} catch (final Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	public static void main(final String[] args) throws Exception {
-		final Instances data = loadDataset(WEATHER_NOMINAL);
-		final Apriori apriori = getApriori();
-
-		apriori.buildAssociations(data);
-		// TODO keine Ahnung
+	private static void printApriori(final Apriori apriori) {
+		System.out.println(apriori);
+		System.out.println(apriori.getAssociationRules().getRules().size());
 	}
 
-	private static Apriori getApriori(final double lowerBoundMinSupport, final double minMetric) {
+	private static Apriori getApriori(final Instances data, final double lowerBoundMinSupport, final double minMetric) throws Exception {
 		final Apriori apriori = new Apriori();
 		apriori.setOutputItemSets(true);
 		apriori.setNumRules(NUM_RULES);
 		apriori.setLowerBoundMinSupport(lowerBoundMinSupport);
 		apriori.setMinMetric(minMetric);
+
+		apriori.buildAssociations(data);
+
 		return apriori;
 	}
 
-	private static Apriori getApriori() {
-		return getApriori(0.1, 1.0);
+	private static Apriori getApriori(final Instances data, final int lowerBoundMinSupport, final double minMetric) throws Exception {
+		return getApriori(data, (double) lowerBoundMinSupport / data.size(), minMetric);
+	}
+
+	private AssociationRuleLearning() {
+	}
+
+	private static Apriori getApriori(final Instances data) throws Exception {
+		return getApriori(data, 0.1, 1.0);
+	}
+
+	private static final class EX_5A {
+		public static void main(final String[] args) throws Exception {
+			printApriori(getApriori(WEATHER));
+		}
+	}
+
+	private static final class EX_5B {
+		public static void main(final String[] args) throws Exception {
+			printApriori(getApriori(WEATHER, 2, 0.75));
+		}
+	}
+
+	private static final class EX_5C {
+		public static void main(final String[] args) throws Exception {
+			printApriori(getApriori(SUPERMARKET, 0.25, 0.75));
+		}
 	}
 }
