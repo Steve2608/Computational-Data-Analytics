f27ada2c755af90ab627938a2e754e7ac65c16fc
Stefan Brandl <steve260897@gmail.com>
2020-05-23 21:58:46 +0200

    removed .html5 extension from report scripts

--- reports/UE1/generate_report.sh
+++ reports/UE1/generate_report.sh
@@ -3,7 +3,7 @@
 if [[ "$1" == "pdf" ]]; then
   pandoc report1.md -o report1.pdf --template eisvogel --pdf-engine=xelatex -V fontsize=10pt -V geometry:landscape
 elif [[ "$1" == "html" ]]; then
-  pandoc report1.md -o report1.html --template GitHub.html5 --metadata title="Computational Data Analytics, UE1" --self-contained
+  pandoc report1.md -o report1.html --template GitHub --metadata title="Computational Data Analytics, UE1" --self-contained
 else
   echo "Unknown parameter: $1"
 fi
--- reports/UE2/generate_report.sh
+++ reports/UE2/generate_report.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 if [[ "$1" == "pdf" ]]; then
-  pandoc report2.md -o report2.pdf --template eisvogel --pdf-engine=xelatex -V fontsize=10pt -V geometry:landscape
+  pandoc report2.md -o report2.pdf --template eisvogel --pdf-engine=xelatex -V fontsize=10.5pt
+
 elif [[ "$1" == "html" ]]; then
-  pandoc report2.md -o report2.html --template GitHub.html5 --metadata title="Computational Data Analytics, UE2" --self-contained
+  pandoc report2.md -o report2.html --template GitHub --metadata title="Computational Data Analytics, UE2" --self-contained
 else
   echo "Unknown parameter: $1"
 fi
